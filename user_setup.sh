9d7685d4605c58a09a5861a2a9bd2907584951ae
Udi <udirom@gmail.com>
2021-05-09 14:03:37 +0300

    having fun

---
+++ user_setup.sh
@@ -0,0 +1,46 @@
+asdf plugin add python
+asdf install python latest
+pip3 install ipython pipenv
+
+asdf plugin add poetry
+asdf install poetry latest
+
+asdf plugin add nodejs
+asdf install nodejs latest
+npm i -g typescript lerna
+
+asdf plugin add exa
+asdf install exa latest
+
+asdf plugin add sqlite
+asdf install sqlite latest
+
+asdf plugin add minikube
+asdf install minikube latest
+ 
+asdf plugin add kubectl
+asdf install kubectl latest
+
+asdf plugin add helm
+asdf install helm latest
+
+asdf plugin add awscli
+asdf install awscli latest
+
+asdf plugin add terraform
+asdf install terraform latest
+
+asdf plugin add fd
+asdf install fd latest
+
+asdf plugin add fzf
+asdf install fzf latest
+
+asdf plugin add vim
+asdf install vim latest
+
+asdf plugin add tmux
+asdf install tmux latest
+
+asdf plugin add rust
+asdf install rust latest
