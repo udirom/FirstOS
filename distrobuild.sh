91cdd3fb1800b8c9f237744fca11a4d568366162
Udi Romano <udi@firstdag.com>
2021-05-12 15:02:03 +0300

    doing stuff

--- distrobuild.sh
+++ distrobuild.sh
@@ -5,44 +5,7 @@ sudo apt update && sudo apt upgrade -y
 
 
 #Install from apt repos
-sudo apt install -y --no-install-recommends \
-    build-essential \
-	ca-certificates \
-	gnupg \
-	lsb-release \
-	software-properties-common \
-	make \
-    git \
-    curl \
-    wget \
-	easy-rsa \
-    software-properties-common \
-    apt-transport-https \
-	# Python
-	python3-pip \
-	python3-venv \
-	python3-testresources \
-	# Deps to build python with pyenv
-	python3-dev \
-	libssl-dev \
-	libffi-dev \
-	zlib1g-dev \
-	libbz2-dev \
-	libreadline-dev \
-	libsqlite3-dev \
-	llvm \
-	libncurses5-dev \
-	xz-utils \
-	tk-dev \
-	libxml2-dev \
-	libxmlsec1-dev \
-	libffi-dev \
-	liblzma-dev \
-	# Shell utils
-	tig \
-	fd-find \
-	jq \
-	network-manager-openvpn
+sudo apt install -y --no-install-recommends gparted fd-find build-essential ca-certificates gnupg lsb-release software-properties-common make git curl wget easy-rsa software-properties-common apt-transport-https python3-pip python3-venv python3-testresources python3-dev libssl-dev libffi-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev tig fd-find jq network-manager-openvpn zsh fish fzf tmux vim autojump
 
 #Install from 3rd party repos
 ##Install Brave
@@ -55,6 +18,11 @@ sudo apt update
 
 sudo apt install brave-browser -y
 
+## Timeshift
+sudo add-apt-repository -y ppa:teejee2008/ppa
+sudo apt update
+sudo apt install timeshift
+
 #Manual installs
 ## Install AppImageLauncher
 wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher_2.2.0-travis995.0f91801.bionic_amd64.deb
@@ -64,49 +32,21 @@ sudo apt install ./appimagelauncher_2.2.0-travis995.0f91801.bionic_amd64.deb -y
 rm appimagelauncher_2.2.0-travis995.0f91801.bionic_amd64.deb
 
 
+## exa
+wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip
+unzip exa-linux-x86_64-v0.10.1.zip -d exa
+cp exa/bin/exa /usr/local/bin/
+rm -rf exa
+
 ## Install Mailspring
 wget https://github.com/Foundry376/Mailspring/releases/download/1.9.1/mailspring-1.9.1-amd64.deb
 sudo apt install ./mailspring-1.9.1-amd64.deb -y
 rm mailspring-1.9.1-amd64.deb
 
-
-#Install Flatpaks
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo apt update 
-
-sudo flatpak install -y flathub com.jetbrains.IntelliJ-IDEA-Ultimate \
-	com.jetbrains.DataGrip \
-	com.spotify.Client \
-	io.bit3.WhatsAppQT \
-	org.fedoraproject.MediaWriter \
-	us.zoom.Zoom \
-	com.slack.Slack \
-	com.simplenote.Simplenote \
-	com.visualstudio.code \
-	com.transmissionbt.Transmission \
-	org.pulseaudio.pavucontrol \
-	org.gnome.Boxes
-
-
-sudo flatpak uninstall --unused
-
-#Install asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-cd ~/.asdf
-git checkout "$(git describe --abbrev=0 --tags)"
-cd ..
-cp -R .asdf /etc/skel
-
-echo '. $HOME/.asdf/asdf.sh' | tee -a ~/.bashrc /etc/skel/.bashrc
-echo '. $HOME/.asdf/completions/asdf.bash' | tee -a ~/.bashrc /etc/skel/.bashrc
-
-
 #Install docker
 sudo apt-get remove -y docker docker-engine docker.io containerd runc
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 
@@ -124,4 +64,7 @@ cd fonts
 cd ..
 rm -Rf fonts
 sudo fc-cache -f -v
+sudo apt autoremove
 
+#Install starship
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
--- user_setup.sh
+++ user_setup.sh
@@ -1,11 +1,19 @@
 # Add user to the docker group
 sudo usermod -aG docker $USER
 
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
+
+# TODO: Install dotfiles
+
+#ln -s .oh-my-zsh ~/.oh-my-zsh
+#ln -s .zshrc ~/.zshrc
+
+#source ~/.zshrc
+
 #asdf
 asdf plugin add python
 asdf install python latest
 asdf global python $(asdf latest python)
-source ~/.bashrc
 
 pip3 install ipython pipenv
 
@@ -17,11 +25,6 @@ asdf plugin add nodejs
 asdf install nodejs latest
 asdf global nodejs $(asdf latest nodejs)
 npm i -g typescript lerna
-
-#colorls
-asdf plugin add exa
-asdf install exa latest
-asdf global exa $(asdf latest exa)
 
 asdf plugin add sqlite
 asdf install sqlite latest
@@ -47,46 +50,21 @@ asdf plugin add terraform
 asdf install terraform latest
 asdf global terraform $(asdf latest terraform)
 
-asdf plugin add fd
-asdf install fd latest
-asdf global fd $(asdf latest fd)
-
-asdf plugin add fzf
-asdf install fzf latest
-asdf global fzf $(asdf latest fzf)
-
-asdf plugin add vim
-asdf install vim latest
-asdf global vim $(asdf latest vim)
-
-asdf plugin add tmux
-asdf install tmux latest
-asdf global tmux $(asdf latest tmux)
-
 asdf plugin add rust
 asdf install rust latest
 asdf global rust $(asdf latest rust)
 
-asdf plugin add starship
-asdf install starship latest
-asdf global starship $(asdf latest starship)
-echo 'eval "$(starship init bash)"' >> ~/.bashrc
-source ~/.bashrc
 
-# bash it + activate plugins
-#git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-#~/.bash_it/install.sh
-#source ~/.bashrc
-#bash-it enable plugin history fzf history-search git docker docker-compose aws autojump
-#bashit enable alias git apt curl node vim docker docker-compose kubectl
-#bash-it enable completion rustup pip3 pipenv tmux npm lerna helm cargo awscli git docker docker-compose
-#source ~/.bashrc
-
-#color ls + aliases
-
-# firefox addons
-# clipboard manager
-#password manager?
-
-
-
+# Flatpaks
+sudo flatpak install -y flathub com.jetbrains.IntelliJ-IDEA-Ultimate \
+	com.jetbrains.DataGrip \
+	com.spotify.Client \
+	io.bit3.WhatsAppQT \
+	org.fedoraproject.MediaWriter \
+	us.zoom.Zoom \
+	com.slack.Slack \
+	com.simplenote.Simplenote \
+	com.visualstudio.code \
+	com.transmissionbt.Transmission \
+	org.pulseaudio.pavucontrol \ 
+    org.telegram.desktop
