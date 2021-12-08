e27bb4ba60b56cf8a5fb8db37d63c9b5cb681835
Udi Romano <udiromano@Udis-MacBook-Pro.local>
2021-12-08 18:12:35 +0200

    Mac installation

---
+++ mac_install.sh
@@ -0,0 +1,66 @@
+#!/bin/zsh
+
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd -P)
+source $SCRIPT_DIR/functions.sh
+
+if [ "$(uname)" != "Darwin" ]; then
+    error "Not a mac computer, exiting..."
+    exit
+fi
+
+info "Docker needs to be installed manually at this point"
+
+if ! command -v brew &> /dev/null
+then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/udiromano/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+brew install gh \
+             git \
+             micro \
+             ccze \
+             rar \
+             fd \
+             wget \
+             easy-rsa \
+             tig \
+             jq \
+             fzf \
+             tmux \
+             multitail \
+             bat \
+             ranger \
+             autojump \
+             glances \
+             cmake \
+             lsd \
+             neofetch \
+             asdf
+
+brew install --cask firefox \
+                    bitwarden \
+                    pycharm \
+                    webstorm \
+                    visual-studio-code \
+                    datagrip \
+                    spotify \
+                    chromium \
+                    whatsapp \
+                    slack \
+                    iterm2
+
+
+curl -sLo $HOME/Library/Fonts/MesloLGS\ NF\ Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+curl -sLo $HOME/Library/Fonts/MesloLGS\ NF\ Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+curl -sLo $HOME/Library/Fonts/MesloLGS\ NF\ Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+curl -sLo $HOME/Library/Fonts/MesloLGS\ NF\ Bold\ Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+
+if ! command -v starship &> /dev/null
+then
+	echo "Installing starship prompt"
+	sudo sh -c "$(curl -fsSL https://starship.rs/install.sh)" "" -y
+fi
+
+zsh -c "$SCRIPT_DIR/mac_user_setup.sh"
---
+++ mac_user_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd -P)
+
+echo "Install oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+mv ~/.zshrc ~/.old-zshrc
+ln -s $SCRIPT_DIR/dotfiles/.mac_zshrc ~/.zshrc
+
+echo "Install oh-my-zsh plugins"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo "Installing asdf dev env"
+source "$SCRIPT_DIR/asdf.sh"
